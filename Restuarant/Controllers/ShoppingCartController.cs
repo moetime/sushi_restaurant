7d3b2f9336ff320fcc619349ef9a4ac12f80bd9e
macravenduarte <duarte.marco242@gmail.com>
2016-08-12 19:24:24 -0400

    shopping cart controller

---
+++ Restuarant/Controllers/ShoppingCartController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant.Models;
+using Restaurant.ViewModels;
+
+namespace Restaurant.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        ShoppingCartContext shoppingCartDB = new ShoppingCartContext();
+        SushiContext sushiItemsDB = new SushiContext();
+        //
+        // GET: /ShoppingCart/
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Set up our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+            // Return the view
+            return View(viewModel);
+        }
+        //
+        // GET: /Store/AddToCart/5
+        public ActionResult AddToCart(int id)
+        {
+            // Retrieve the food item from the database
+            var addedFoodItem = sushiItemsDB.FoodItems
+                .Single(FoodItem => FoodItem.ID == id);
+
+            // Add it to the shopping cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedFoodItem);
+
+            // Go back to the main store page for more shopping
+            return RedirectToAction("Index");
+        }
+        //
+        // AJAX: /ShoppingCart/RemoveFromCart/5
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            // Remove the item from the cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Get the name of the album to display confirmation
+            string foodItemName = shoppingCartDB.Carts
+                .Single(FoodItem => FoodItem.CartID == id).FoodItem.Name;
+
+            // Remove from cart
+            int itemCount = cart.RemoveFromCart(id);
+
+            // Display the confirmation message
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(foodItemName) +
+                    " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+        //
+        // GET: /ShoppingCart/CartSummary
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
---
+++ Restuarant/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Restaurant.Models;
+
+namespace Restaurant.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public virtual List<Cart> CartItems { get; set; }
+        public virtual decimal CartTotal { get; set; }
+    }
+}
