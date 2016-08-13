061a72c8a6ca4cc4009c0678ef9e30f6fdba4ab8
macravenduarte <duarte.marco242@gmail.com>
2016-08-12 21:39:59 -0400

    jquery added and type script

--- Restuarant/App_Start/BundleConfig.cs
+++ Restuarant/App_Start/BundleConfig.cs
@@ -17,16 +17,19 @@ namespace Restaurant
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*" ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js" ));
+
+            bundles.Add(new ScriptBundle("~/bundles/shoppingCart").Include(
+                    "~/Scripts/app.js" ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css" ));
         }
     }
 }
