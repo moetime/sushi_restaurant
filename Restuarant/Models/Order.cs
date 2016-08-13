27606e4549b020b2a9526e7bb91951ecca09ed96
averyirin <averyirin@gmail.com>
2016-08-13 14:28:11 -0400

    Created checkout functionality

--- Restuarant/Models/Order.cs
+++ Restuarant/Models/Order.cs
@@ -2,16 +2,63 @@ namespace Restaurant.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Web.Mvc;
 
+    [Bind(Exclude = "OrderID")]
     public partial class Order
     {
-        
 
+        [ScaffoldColumn(false)]
         public int OrderID { get; set; }
+        [ScaffoldColumn(false)]
+        public System.DateTime OrderDate { get; set; }
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [StringLength(160)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(70)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(40)]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Province is required")]
+        [StringLength(40)]
+        public string Province { get; set; }
+        [Required(ErrorMessage = "Postal Code is required")]
+        [DisplayName("Postal Code")]
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(40)]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Phone is required")]
+        [StringLength(24)]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Email Address is required")]
+        [DisplayName("Email Address")]
 
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [ScaffoldColumn(false)]
+        public decimal Total { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        /*
+        public int OrderID { get; set; }
+        
         public string Username { get; set; }
 
         public string FirstName { get; set; }
@@ -37,5 +84,6 @@ namespace Restaurant.Models
         public DateTime OrderDate { get; set; }
 
         public virtual List<OrderDetail> OrderDetails { get; set; }
+    */
     }
 }
